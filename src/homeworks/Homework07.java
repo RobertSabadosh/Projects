9712897c6ae85d28b97c46e13936108e7b1ab51e
RobertSabadosh <robert.sabadosh@gmail.com>
2020-07-09 12:48:20 +0200

    New classwork

--- src/homeworks/Homework07.java
+++ src/homeworks/Homework07.java
@@ -3,9 +3,22 @@ package homeworks;
 public class Homework07 {
     public static void main(String[] args) {
         System.out.println(heroChoice("left"));
+        System.out.println(heroChoice("right"));
+        System.out.println(heroChoice("straight"));
         split();
-        int speed = 101;
-        speedLimit(speed);
+        int speed = 129;
+        split();
+        System.out.println(speedLimit2(46));
+        System.out.println(speedLimit2(56));
+        System.out.println(speedLimit2(80));
+        System.out.println(speedLimit2(120));
+        split();
+        System.out.println(heroChoice2(1));
+        System.out.println(heroChoice2(2));
+        System.out.println(heroChoice2(3));
+        System.out.println(heroChoice2(4));
+        split();
+
     }
 
     private static void split() {
@@ -31,12 +44,57 @@ public class Homework07 {
         return path;
     }
 
+    public static String heroChoice2(int way){
+        String result = "";
+        if (way==1){
+            result = "Коня потеряешь";
+        }else if (way==2){
+            result = "Жизнь потеряешь";
+        }else if (way==3){
+            result = "Счастье обретёшь";
+        }else {
+            result = "Спроси ещё";
+        }
+        return result;
+    }
+
+    public static String speedLimit3(int speed) {
+        String result = "";
+        if (speed <= 50) {
+            result = "Нет нарушений";
+        } else if (speed <= 65) {
+            result = "устное порицание и лекция на 5 минут";
+        }else if(speed<=100) {
+            result =  "40 Евро штраф";
+        }else if (speed<=130) {
+            result =  "штраф 500 евро";
+        }else{
+            result =   "штраф в 1000 евро, конфискация прав на 3 года и конфискация транспортного средства";
+        }
+        return result;
+    }
+
+
+    public static String speedLimit2(int speed) {
+        if (speed <= 50) {
+            return "Нет нарушений";
+        } else if (speed <= 65) {
+            return "устное порицание и лекция на 5 минут";
+        }else if(speed<=100) {
+            return "40 Евро штраф";
+        }else if (speed<=130) {
+            return "штраф 500 евро";
+        }else{
+           return  "штраф в 1000 евро, конфискация прав на 3 года и конфискация транспортного средства";
+        }
+    }
+
     public static void speedLimit(int speed) {
 
         if (speed <= 50) {
             System.out.println("Нет нарушений");
 
-        } else if (speed >= 50 && speed <= 65) { // Java не нравится что здесь написано speed >= 50, но по условию должно быть
+        } else if (speed <= 65) { // Java не нравится что здесь написано speed >= 50, но по условию должно быть
             System.out.println("устное порицание и лекция на 5 минут");
 
         } else if (speed >= 100 && speed <= 130) {
---
+++ src/lesson10/WarmUpOne.java
@@ -0,0 +1,50 @@
+package lesson10;
+
+public class WarmUpOne {
+    public static void main(String[] args) {
+        System.out.println(sleepIn(false, false));// expecting true
+        System.out.println(sleepIn(true, false));// expecting false
+        System.out.println(sleepIn(false, true));//expecting true
+        split();
+        System.out.println(diff21(19));
+        System.out.println(diff21(10));
+        System.out.println(diff21(21));
+        split();
+        System.out.println(missingChar("kitten", 1)); // expecting "ktten"
+        System.out.println(missingChar("kitten", 0)); // expecting "itten"
+        System.out.println(missingChar("kitten", 4)); // expecting "kittn"
+        split();
+        System.out.println(backAround("cat"));
+        System.out.println(backAround("Hello"));
+        System.out.println(backAround("a"));
+    }
+
+    public static void split() {
+        System.out.println("---------------------------------");
+    }
+
+    public static boolean sleepIn(boolean weekday, boolean vacation) {
+        return !weekday || vacation;
+    }
+
+    public static double diff21(int n) {
+        if (n > 21) {
+            return n - (21 * 2);
+        } else {
+            return Math.abs(n - 21);
+        }
+    }
+
+    public static String missingChar(String s, int n) {
+        String output = "";
+        output = s.substring(0, n) + s.substring(n + 1);
+        return output;
+    }
+
+    public static String backAround(String str) {
+        String output = "";
+        int lastSymbol = str.length() - 1;
+        output = str.substring(lastSymbol);
+        return output + str + output;
+    }
+}
