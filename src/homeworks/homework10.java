8d2d621c72b9a31633e432cb690b41020df26132
RobertSabadosh <robert.sabadosh@gmail.com>
2020-07-09 18:33:06 +0200

    Homework from lesson10

---
+++ src/homeworks/homework10.java
@@ -0,0 +1,44 @@
+package homeworks;
+
+public class homework10 {
+    public static void main(String[] args) {
+        System.out.println(startWithHi("hi there")); // expecting true
+        System.out.println(startWithHi("hi"));       // expecting true
+        System.out.println(startWithHi("hello hi")); // expecting false
+        split();
+        /*System.out.println(startWithHi2("hi there"));
+        System.out.println(startWithHi2("hi"));
+        System.out.println(startWithHi2("hello hi"));*/
+        split();
+        System.out.println(hasTeen(13, 20, 10)); // expecting true
+        System.out.println(hasTeen(10, 19, 10)); // expecting true
+        System.out.println(hasTeen(20, 10, 13)); // expecting true
+    }
+
+    public static boolean startWithHi(String str) {
+        if (str.length() < 2)
+            return false;
+        
+        String firstTwo = str.substring(0, 2);
+        if (firstTwo.equals("hi")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean hasTeen(int a, int b, int c) {
+        return (a >= 13 && a <= 20 || b >= 13 && b <= 20 || c >= 13 && c <= 20);
+    }
+
+   /* public static boolean startWithHi2(String str) {
+        if (str.length() < 2)
+            return false;
+        String firstTwo = str.substring(0, 2);
+        return firstTwo.equals("hi");
+    }*/
+
+    public static void split() {
+        System.out.println("------------------------");
+    }
+}
