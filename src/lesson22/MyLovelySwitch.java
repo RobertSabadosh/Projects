bace607e5218f3f58d65944320aee76d6c12c349
RobertSabadosh <robert.sabadosh@gmail.com>
2020-07-24 13:17:55 +0200

    Classwork22

--- src/lesson22/MyLovelySwitch.java
+++ src/lesson22/MyLovelySwitch.java
@@ -1,4 +1,26 @@
 package lesson22;
 
 public class MyLovelySwitch {
+
+    public static int cityWeather(String city, String weekday) {
+        int degreeUp = 0;
+        switch (weekday) {
+            case "Monday":
+                return 0;
+            case "Tuesday":
+                return 1;
+            case "Wednesday":
+                return 2;
+            case "Thursday":
+                return 3;
+            case "Friday":
+                return 4;
+            case "Saturday":
+                return 5;
+            case "Sunday":
+                return 6;
+            default:
+                return 0;
+        }
+    }
 }
--- src/lesson22/MyLovelyTernarOperator.java
+++ src/lesson22/MyLovelyTernarOperator.java
@@ -1,4 +1,30 @@
 package lesson22;
 
 public class MyLovelyTernarOperator {
+
+    public static void main(String[] args) {
+
+        test1();
+
+    }
+
+    private static void test1() {
+        int a = 10;
+        int b = 20;
+        int c;
+        if (a > b) {
+            c = a;
+        } else {
+            c = b;
+        }
+        System.out.println("c из if-else оператора" + c);
+    }
+
+    public static void test2() {
+        int a = 10;
+        int b = 20;
+        int c;
+        c = (a > b) ? a : b;
+        System.out.println("c из тернарного оператора" + c);
+    }
 }
