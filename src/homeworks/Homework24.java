b62856c1a3278f9eb6320cfa5e62eaaa2820e885
RobertSabadosh <robert.sabadosh@gmail.com>
2020-07-27 21:13:11 +0200

    HW24

---
+++ src/homeworks/Homework24.java
@@ -0,0 +1,50 @@
+package homeworks;
+
+import static homeworks.UniversalMethod.split;
+
+public class Homework24 {
+
+    public static void main(String[] args) {
+
+        System.out.println(blueTicket(9, 1, 0));// 10
+        System.out.println(blueTicket(9, 2, 0)); // 0
+        System.out.println(blueTicket(6, 1, 4)); // 10
+        split();
+        System.out.println(dateFashion(5, 10)); // 2
+        System.out.println(dateFashion(5, 2)); // 0
+        System.out.println(dateFashion(5, 5)); // 1
+        split();
+        System.out.println(sortaSum(3, 4)); // 7
+        System.out.println(sortaSum(9, 4)); // 20
+        System.out.println(sortaSum(10, 11)); // 21
+    }
+
+    private static int blueTicket(int a, int b, int c) {
+        int ab = a + b;
+        int bc = b + c;
+        int ac = a + c;
+
+        if (ab == 10 || bc == 10 || ac == 10) {
+            return 10;
+        }else if (ab - bc == 10 || ab - ac == 10) {
+            return 5;
+        }
+        return 0;
+    }
+
+    private static int dateFashion(int you, int date) {
+        if (you <= 2 || date <= 2) {
+            return 0;
+        } else if (you >= 8 || date >= 8) {
+            return 2;
+        }
+            return 1;
+    }
+
+    private static int sortaSum(int a, int b) {
+        if (a + b >= 10 && a + b <= 19) {
+            return 20;
+        }
+        return a + b;
+    }
+}
