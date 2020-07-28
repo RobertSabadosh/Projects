85cf3f674851530ce463547b7e2bbeaec487af4d
RobertSabadosh <robert.sabadosh@gmail.com>
2020-07-28 10:18:34 +0200

    HW24

--- src/homeworks/Homework24.java
+++ src/homeworks/Homework24.java
@@ -9,6 +9,7 @@ public class Homework24 {
         System.out.println(blueTicket(9, 1, 0));// 10
         System.out.println(blueTicket(9, 2, 0)); // 0
         System.out.println(blueTicket(6, 1, 4)); // 10
+        System.out.println(blueTicket(13, 7, 3)); // 5
         split();
         System.out.println(dateFashion(5, 10)); // 2
         System.out.println(dateFashion(5, 2)); // 0
@@ -19,20 +20,20 @@ public class Homework24 {
         System.out.println(sortaSum(10, 11)); // 21
     }
 
-    private static int blueTicket(int a, int b, int c) {
+    public static int blueTicket(int a, int b, int c) {
         int ab = a + b;
         int bc = b + c;
         int ac = a + c;
-
-        if (ab == 10 || bc == 10 || ac == 10) {
-            return 10;
-        } else if (ab - bc == 10 || ab - ac == 10) {
+        
+        if (ab == bc + 10 || ab == ac + 10) {
             return 5;
+        } else if (ab == 10 || bc == 10 || ac == 10) {
+            return 10;
         }
         return 0;
     }
 
-    private static int dateFashion(int you, int date) {
+    public static int dateFashion(int you, int date) {
         if (you <= 2 || date <= 2) {
             return 0;
         } else if (you >= 8 || date >= 8) {
@@ -41,7 +42,7 @@ public class Homework24 {
         return 1;
     }
 
-    private static int sortaSum(int a, int b) {
+    public static int sortaSum(int a, int b) {
         if (a + b >= 10 && a + b <= 19) {
             return 20;
         }
