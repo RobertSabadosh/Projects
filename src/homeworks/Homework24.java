e10d5e27f2295f83314957f26a9f19f836eba319
RobertSabadosh <robert.sabadosh@gmail.com>
2020-07-29 11:14:14 +0200

    Updates

--- src/homeworks/Homework24.java
+++ src/homeworks/Homework24.java
@@ -24,7 +24,7 @@ public class Homework24 {
         int ab = a + b;
         int bc = b + c;
         int ac = a + c;
-        
+
         if (ab == bc + 10 || ab == ac + 10) {
             return 5;
         } else if (ab == 10 || bc == 10 || ac == 10) {
