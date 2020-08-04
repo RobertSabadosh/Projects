17c634a3f7efaad90779c27048fffeef305de0ef
RobertSabadosh <robert.sabadosh@gmail.com>
2020-08-04 20:12:06 +0200

    Update class UniversalMethod

--- src/homeworks/UniversalMethod.java
+++ src/homeworks/UniversalMethod.java
@@ -4,9 +4,26 @@ public class UniversalMethod {
 
     public static void main(String[] args) {
         split();
+        /*int[] myArray = new int[0];
+        myArrayPrint(myArray);*/
     }
 
     public static void split() {
         System.out.println("----------------------------");
+    }
+
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "\t");
+        }
+        System.out.println();
+    }
+
+    public static void myArrayPrint(int[] arrayToPrint) {
+        System.out.println("Длина массива - " + arrayToPrint.length);
+        for (int j : arrayToPrint) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
     }
 }
