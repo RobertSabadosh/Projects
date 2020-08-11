20aa383032337da2db1e95070d7e7fb46ac6fa20
RobertSabadosh <robert.sabadosh@gmail.com>
2020-08-11 10:17:14 +0200

    Lesson - 36

---
+++ src/lesson36/ArrayExerciseMaxTriple.java
@@ -0,0 +1,31 @@
+package lesson36;
+
+public class ArrayExerciseMaxTriple {
+
+    public static void main(String[] args) {
+
+        int[] a1 = new int[]{1, 2, 3};
+        int[] a2 = new int[]{1, 5, 3,};
+        int[] a3 = new int[]{5, 7, 2, 8, 3};
+        System.out.println(maxTriple(a1)); // 3
+        System.out.println(maxTriple(a2)); // 5
+        System.out.println(maxTriple(a3)); // 5
+
+    }
+
+    /*Given an array of ints of odd length, look at the first, last, and middle values in the array and return the largest. The array
+    length will be a least 1.*/
+    public static int maxTriple(int[] arr) {
+        int result = arr[0];
+        int middle = arr[arr.length / 2];
+        int end = arr[arr.length - 1];
+        /*if (middle > result) {
+            result = middle;
+        } if (end> result) {
+            result = end;
+        }*/
+        result = Math.max(result, middle);
+        result = Math.max(result, end);
+        return result;
+    }
+}
---
+++ src/lesson36/Classwork36.java
@@ -0,0 +1,66 @@
+package lesson36;
+
+import java.util.Arrays;
+
+import static homeworks.UniversalMethod.split;
+
+public class Classwork36 {
+
+    public static void main(String[] args) {
+
+        fizzBuzz(); // "Fizz"
+//        System.out.println(fizzBuzz(5)); // "Buzz"
+//        System.out.println(fizzBuzz(15)); // "FizzBuzz"
+        System.out.println(Arrays.toString(fizzBuzz3(1, 6)));
+        split();
+        System.out.println(Arrays.toString(fizzBuzz3(15, 25)));
+
+    }
+
+    public static void fizzBuzz() {
+        for (int i = 0; i <= 100; i++) {
+            System.out.println(i);
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void fizzBuzz2(int min, int max) {
+        for (int i = min; i <= max; i++) {
+            System.out.println(i);
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static String[] fizzBuzz3(int min, int max) {
+        int range = max - min;
+        String[] output = new String[range];
+        for (int i = 0; i < range; i++) {
+            if ((i) % 3 == 0 && min % 5 == 0) {
+                output[i] = "FizzBuzz";
+            } else if (min % 3 == 0) {
+                output[i] = "Fizz";
+            } else if (min % 5 == 0) {
+                output[i] = "Buzz";
+            } else {
+                output[i] = String.valueOf(min++);
+            }
+        }
+        return output;
+    }
+}
