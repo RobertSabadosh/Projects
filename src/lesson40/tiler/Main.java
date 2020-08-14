31e35f7570b53f68a1085037a126d590896a4186
RobertSabadosh <robert.sabadosh@gmail.com>
2020-08-14 15:11:37 +0200

    Lesson - 40

---
+++ src/lesson40/tiler/Main.java
@@ -0,0 +1,11 @@
+package lesson40.tiler;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double roomWidth = 5.5; // метры
+        double roomLength = 7.5; // метры
+        double tilesPrice = 3.0;
+        TilesApplication.start(roomWidth, roomLength, tilesPrice);
+    }
+}
