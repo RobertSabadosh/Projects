5d7ad3f0e479d9e773d9881ca4968d06209e2b74
RobertSabadosh <robert.sabadosh@gmail.com>
2020-08-25 22:58:00 +0200

    Test

---
+++ src/javaScript/script.js
@@ -0,0 +1,11 @@
+/*
+var str = "Hello World!"
+alert(str);
+console.log(str.toUpperCase());*/
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+
+alert("Please push the 'OK' button");
+console.log('Hello World!!!!');
